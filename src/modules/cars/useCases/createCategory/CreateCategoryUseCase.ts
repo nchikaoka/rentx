13a63998cfea618ba2078f1e89bc54afcc9f4ba3
nchikaoka <natan.chikaoka@gmail.com>
2021-11-28 17:46:03 -0300

    modified 'Category' model to DB entity, initiated the reafactoring of the implmentations, controllers, use cases and routes.

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -7,9 +7,11 @@ interface IRequest {
 
 class CreateCategoryUseCase {
   // eslint-disable-next-line prettier/prettier
-  constructor(private categoriesRepository: ICategoriesRepository) { }
-  execute({ name, description }: IRequest): void {
-    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+  async execute({ name, description }: IRequest): Promise<void> {
+    const categoryAlreadyExists = await this.categoriesRepository.findByName(
+      name
+    );
 
     if (categoryAlreadyExists) {
       throw new Error("Category already exists");
